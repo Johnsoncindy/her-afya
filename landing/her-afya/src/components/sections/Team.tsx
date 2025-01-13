3431c4b4b761bd18f1ce87cfbf640df89c0d4bfb
Geitodyu Henrique Crusoe <crusoehenrique3@gmail.com>
2025-01-13 15:26:43 +0300

    update team

--- landing/her-afya/src/components/sections/Team.tsx
+++ landing/her-afya/src/components/sections/Team.tsx
@@ -108,7 +108,7 @@ export default function Team() {
                         src={founder.image}
                         alt={founder.name}
                         fill
-                        className="object-cover"
+                        className='object-contain'
                       />
                     </div>
                     <div className="text-center md:text-left">
@@ -166,7 +166,7 @@ export default function Team() {
                           src={member.image}
                           alt={member.name}
                           fill
-                          className="object-cover"
+                          className='object-contain'
                         />
                       </div>
                       <div className="text-center md:text-left">
@@ -225,7 +225,7 @@ export default function Team() {
                           src={advisor.image}
                           alt={advisor.name}
                           fill
-                          className="object-cover"
+                          className='object-contain'
                         />
                       </div>
                       <div className="text-center md:text-left">
